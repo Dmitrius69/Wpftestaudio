d3a320e3a0723654384a77b3753a94b2072f3c64
Dmitrius69 <dmitrius69@gmail.com>
2019-03-01 18:01:52 +0300

    Добавьте файлы проекта.

---
+++ Wpftestaudio/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using WPFSoundVisualizationLib;
+
+
+namespace Wpftestaudio
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Opensound_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+            openDialog.Filter = "(*.mp3)|*.mp3";
+            if (openDialog.ShowDialog() == true)
+            {
+                //NAudioEngine.Instance.OpenFile(openDialog.FileName);
+                FileText.Text = openDialog.FileName;
+            }
+        }
+    }
+}
